d70b7c80a3907bb56550da1a1b74433adc444cc7
Kunal Kumar <kumarkunal8759@gmail.com>
2024-08-06 22:47:41 +0530

    added Seeders

---
+++ src/seeders/20240806160419-AirplaneSeeder.js
@@ -0,0 +1,45 @@
+'use strict';
+
+//@type {import('sequelize-cli').Migration} */
+
+const { Op } = require('@sequelize/core');
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('Airplanes',[
+      {
+        ModelNo:'airbus-b22',
+        capacity:111,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        ModelNo:'airbus-b11',
+        capacity:222,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    await queryInterface.bulkDelete('Airplanes', {[Op.or]:[{ModelNo:'airbus-b22'},{ModelNo:'null'}]});
+  }
+};
